5eb6fcfedc0f7cddd291a450e8b99866da12cdc0
Michael Shimniok <shimniok@gmail.com>
2015-03-04 14:06:41 -0700

    finished getting it working thru proxy

--- rover.js
+++ rover.js
@@ -1,63 +1,33 @@
-// Resize buttons and image
-//
-function doResize() {
-  var ww;
-  var wh;
-  var margin = 60;
-  var ratio = 320/240;
-  var winportion = 0.6; // portion of vertical space taken up by image
+// Rover Jquery
+// Michael Shimniok
 
-  if (window.orientation == 90 || window.orientation == -90) {
-    ww = $(window).height();
-    wh = $(window).width()-margin;
-  } else {
-    ww = $(document).width();
-    wh = $(document).height()-margin;
-  }
-
-  var iw = ww - margin*2;
-  var ih = wh*winportion - margin;
-
-  // Resize Image Stream
-  if (ih * ratio > iw) {
-    $("img#imagestream").height(iw/ratio);
-    $("img#imagestream").width(iw);
-  } else {
-    $("img#imagestream").width(ih*ratio);
-    $("img#imagestream").height(ih);
-  }
-
-  // Resize Table
-  $("table.button").width(wh - ih);
-  $("table.button").height(wh - ih);
-}
-
-
-$(window).load(function() {
-  doResize();
-  $(window).on('resize', doResize);
-  $(window).on('orientationchange', doResize);
-});
+var GPIO = Array(54);
 
 
 $(document).ready(function() {
+	var left, right, top, button;
+
   $("img#imagestream").attr('src', 'http://' + window.location.hostname + ':8080/?action=stream');
-});
 
-
-webiopi().ready(function() {
-  var left, right, top, button;
+	// init GPIOs
+	for (var i=0; i < GPIO.length; i++) {
+		var gpio = Object();
+		gpio.value = 0;
+		gpio.func = "IN";
+		gpio.mapped = false;
+		GPIO[i] = gpio;
+	}
 
   top = $( "#topbuttons" );
   left = $( "#leftbuttons" );
   right = $( "#rightbuttons" );
 
-  webiopi().setFunction(11, "OUT"); // mode
-  webiopi().digitalWrite(11, 0);    // IN-IN mode
-  webiopi().setFunction(9, "OUT");  // a phase
-  webiopi().setFunction(10, "OUT"); // a enable
-  webiopi().setFunction(21, "OUT"); // b phase
-  webiopi().setFunction(22, "OUT"); // b enable
+  setFunction(11, "OUT"); // mode
+  digitalWrite(11, 0);    // IN-IN mode
+  setFunction(9, "OUT");  // a phase
+  setFunction(10, "OUT"); // a enable
+  setFunction(21, "OUT"); // b phase
+  setFunction(22, "OUT"); // b enable
 
   // Note that on iDevices, touchstart/touchend come first and
   // mousedown/mouseup are also fired, so you have to disable
@@ -131,45 +101,108 @@ function sign(x) {
 
 
 function fwd() {
-  webiopi().digitalWrite(11, 0);
-
-  webiopi().digitalWrite(9, 0);  // AIN1
-  webiopi().digitalWrite(21, 0); // BIN1
-  webiopi().digitalWrite(10, 1); // AIN2
-  webiopi().digitalWrite(22, 1); // BIN2
+  digitalWrite(11, 0);
+  digitalWrite(9, 0);  // AIN1
+  digitalWrite(21, 0); // BIN1
+  digitalWrite(10, 1); // AIN2
+  digitalWrite(22, 1); // BIN2
 }
 
 function rev() {
-  webiopi().digitalWrite(11, 0);
-  webiopi().digitalWrite(9, 1);	 // AIN1
-  webiopi().digitalWrite(10, 0); // AIN2
-  webiopi().digitalWrite(21, 1); // BIN1
-  webiopi().digitalWrite(22, 0); // BIN2
+  digitalWrite(11, 0);
+  digitalWrite(9, 1);	 // AIN1
+  digitalWrite(10, 0); // AIN2
+  digitalWrite(21, 1); // BIN1
+  digitalWrite(22, 0); // BIN2
 }
 
 function spinLeft() {
-  webiopi().digitalWrite(11, 0);
-  webiopi().digitalWrite(9, 1);	 // AIN1
-  webiopi().digitalWrite(10, 0); // AIN2
-  webiopi().digitalWrite(21, 0); // BIN1
-  webiopi().digitalWrite(22, 1); // BIN2
+  digitalWrite(11, 0);
+  digitalWrite(9, 1);	 // AIN1
+  digitalWrite(10, 0); // AIN2
+  digitalWrite(21, 0); // BIN1
+  digitalWrite(22, 1); // BIN2
 }
 
 function spinRight() {
-  webiopi().digitalWrite(11, 0);
-  webiopi().digitalWrite(9, 0);	 // AIN2
-  webiopi().digitalWrite(10, 1); // AIN2
-  webiopi().digitalWrite(21, 1); // BIN2
-  webiopi().digitalWrite(22, 0); // BIN2
+  digitalWrite(11, 0);
+  digitalWrite(9, 0);	 // AIN2
+  digitalWrite(10, 1); // AIN2
+  digitalWrite(21, 1); // BIN2
+  digitalWrite(22, 0); // BIN2
 }
 
 function stop() {
-  webiopi().digitalWrite(11, 0);
-  webiopi().digitalWrite(9, 0);	 // AIN1
-  webiopi().digitalWrite(10, 0); // AIN2
-  webiopi().digitalWrite(21, 0); // BIN1
-  webiopi().digitalWrite(22, 0); // BIN2
+  digitalWrite(11, 0);
+  digitalWrite(9, 0);	 // AIN1
+  digitalWrite(10, 0); // AIN2
+  digitalWrite(21, 0); // BIN1
+  digitalWrite(22, 0); // BIN2
 }
 
 
+function setFunction(gpio, func) {
+	$.post('/GPIO/' + gpio + "/function/" + func, function(data) {
+		updateFunction(gpio, data);
+	});
+}
 
+
+function digitalWrite(gpio, value) {
+	if (GPIO[gpio].func.toUpperCase() == "OUT") {
+		$.post('/GPIO/' + gpio + "/value/" + value, function(data) {
+			updateValue(gpio, data);
+		});
+	}
+}
+
+function updateFunction(gpio, func) {
+	GPIO[gpio].func = func;
+}
+
+
+function updateValue(gpio, value) {
+  GPIO[gpio].value = value;
+}
+
+
+// Window Resizing
+$(window).load(function() {
+  doResize();
+  $(window).on('resize', doResize);
+  $(window).on('orientationchange', doResize);
+});
+
+
+// Resize buttons and image
+function doResize() {
+  var ww;
+  var wh;
+  var margin = 60;
+  var ratio = 320/240;
+  var winportion = 0.6; // portion of vertical space taken up by image
+
+  if (window.orientation == 90 || window.orientation == -90) {
+    ww = $(window).height();
+    wh = $(window).width()-margin;
+  } else {
+    ww = $(document).width();
+    wh = $(document).height()-margin;
+  }
+
+  var iw = ww - margin*2;
+  var ih = wh*winportion - margin;
+
+  // Resize Image Stream
+  if (ih * ratio > iw) {
+    $("img#imagestream").height(iw/ratio);
+    $("img#imagestream").width(iw);
+  } else {
+    $("img#imagestream").width(ih*ratio);
+    $("img#imagestream").height(ih);
+  }
+
+  // Resize Table
+  $("table.button").width(wh - ih);
+  $("table.button").height(wh - ih);
+}
